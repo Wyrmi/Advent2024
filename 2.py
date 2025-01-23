49c22c3b224e8e74cdbf93a9292e43973c7d1812
Wyrmi <cyan.wyrmie@gmail.com>
2025-01-23 10:58:28 +0200

    day 2 part 2

--- 2.py
+++ 2.py
@@ -1,21 +1,42 @@
 import fileinput
 filepath = 'data/2.txt'
 sum = 0
-
+ptwo = []
 for line in fileinput.input(filepath, inplace = False):
     row = line.split()
     isOk = True
-    decends = True
     first = True
     for i in range(0, len(row)):
         row[i] = int(row[i])
-    if (row[0] < row[1]):
-        decends = False
+    
+    if (row == sorted(row) or row == sorted(row, reverse=True)):
+        for i in range(0, len(row) -1):
+            if row[i] == row[i +1] or abs(row[i] - row[i +1]) > 3 or (row == sorted(row, reverse=True) and row[i] < row[i +1] or row == sorted(row) and row[i] > row[i +1]):
+                isOk = False
+                break
     else:
-        decends = True
-    for i in range(0, len(row) -1):
-        if row[i] == row[i +1] or abs(row[i] - row[i +1]) > 3 or (decends and row[i] < row[i +1] or not decends and row[i] > row[i +1]):
-            isOk = False
+        isOk = False
     if isOk:
         sum += 1
+    else:
+        ptwo.append(row)
+print(sum)
+
+#part 2
+
+for j in ptwo:
+    for i in range(0, len(j)):
+        temp = j.copy()
+        temp.pop(i)
+        isOk = True
+        if (temp == sorted(temp) or temp == sorted(temp, reverse=True)):
+            for x in range(0, len(temp) -1):
+                if temp[x] == temp[x +1] or abs(temp[x] - temp[x +1]) > 3 or (temp == sorted(temp, reverse=True) and temp[x] < temp[x +1] or temp == sorted(temp) and temp[x] > temp[x +1]):
+                    isOk = False
+                    break
+        else:
+            isOk = False
+        if isOk:
+            sum += 1
+            break
 print(sum)
