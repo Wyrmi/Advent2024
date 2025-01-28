7f62890b549f004bb84a0210123099977a450400
Wyrmi <cyan.wyrmie@gmail.com>
2025-01-28 13:28:42 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,9 +5,10 @@ sum = 0
 valids = []
 for line in fileinput.input(filepath, inplace = False):
     #row = line.split()
-    valids = re.findall("mul\([0-9]+,[0-9]+\)+", line)
+    valids.extend(re.findall(r"mul\(\d+,\d+\)", line))
 
 for i in valids:
     nums = list(map(int, re.findall(r'\d+', i)))
     sum += nums[0] * nums[1]
+
 print(sum)
